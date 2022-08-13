747b6be7b5a41815699c6b2420c51c5a3f3dd780
Terry Ho <101287576+chung900811@users.noreply.github.com>
2022-08-13 02:27:08 -0700

    Updated fetcher.js

--- page-fetcher/fetcher.js
+++ page-fetcher/fetcher.js
@@ -1,18 +1,15 @@
+const fs = require('fs');
+const URL = process.argv[2];
+const filePath = process.argv[3];
 const request = require('request');
-const fs = require("fs");
 
-const path = process.argv[3];
-const domain = process.argv[2];
-
-request(domain, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  }
-  fs.writeFile(`${path}`, body, function(error) {
-    if (error) {
-      console.log("error:", error);
-    } else {
-      console.log(`Downloaded and saved ${response.headers["content-length"]} bytes to ${path}`);
-    }
-  });
+request(`${URL}`, (error, response, body) => {
+  saveFile(response, body)
 });
+
+const saveFile = function(statusCode, data) {
+  fs.writeFile(`${filePath}`, data, (err) => {
+    if (err) throw err;
+    console.log(`Downloaded and saved ${data.length} bytes to ${filePath}`)
+  });
+}
